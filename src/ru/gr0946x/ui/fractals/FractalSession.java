df4ea81eb62556555f623086eaf43d4fa9303aa3
yotisha <yotisha@yotisha.com>
2026-04-17 15:44:57 +0300

    conflict resolution

--- src/ru/gr0946x/ui/fractals/FractalSession.java
+++ src/ru/gr0946x/ui/fractals/FractalSession.java
@@ -8,25 +8,31 @@ public class FractalSession implements Serializable {
 
     public String type;
 
+    public int fractalIdx;
+
+    public int colorIdx;
+
     public double xMin, xMax, yMin, yMax;
 
     public Double juliaCRe, juliaCIm;
 
+    public boolean dynamicIterationsEnabled;
+    public double dynamicIterationsLastWidth;
+
     public FractalSession() {}
 
-    public FractalSession(String type, double xMin, double xMax, double yMin, double yMax, Complex juliaC) {
+    public FractalSession(String type, int fractalIdx, int colorIdx,
+                          double xMin, double xMax, double yMin, double yMax,
+                          Complex juliaC, boolean dynIterEnabled, double dynIterWidth) {
         this.type = type;
+        this.fractalIdx = fractalIdx;
+        this.colorIdx = colorIdx;
         this.xMin = xMin; this.xMax = xMax; this.yMin = yMin; this.yMax = yMax;
         if (juliaC != null) {
             this.juliaCRe = juliaC.getReal();
             this.juliaCIm = juliaC.getImaginary();
         }
-    }
-
-    public Complex getJuliaC() {
-        if (juliaCRe != null && juliaCIm != null) {
-            return new Complex(juliaCRe, juliaCIm);
-        }
-        return null;
+        this.dynamicIterationsEnabled = dynIterEnabled;
+        this.dynamicIterationsLastWidth = dynIterWidth;
     }
 }
